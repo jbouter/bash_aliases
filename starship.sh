a43a6378f63919d173e61140b9ba920e69d33dff
Jeffrey Bouter <jb@warpnet.nl>
2021-07-08 10:04:49 +0200

    Update starship to support both bash and zsh
    Signed-off-by: Jeffrey Bouter <jb@warpnet.nl>

--- starship.sh
+++ starship.sh
@@ -2,11 +2,17 @@
 # shellcheck disable=SC2034,SC2086
 
 function set_win_title(){
-  if [ "$(basename $PWD)" == "$(id -un)" ]; then
+  if [[ "$(basename $PWD)" == "$(id -un)" ]]; then
     echo -ne "\033]0; $(id -un)@$(hostname): ~ \007"
   else
     echo -ne "\033]0; $(id -un)@$(hostname): $(basename $PWD) \007"
   fi
 }
 
-starship_precmd_user_func="set_win_title"
+# Set the window title
+SHELL=$(basename $SHELL)
+if [[ "$SHELL" == "bash" ]]; then
+  starship_precmd_user_func="set_win_title"
+elif [[ "$SHELL" == "zsh" ]]; then
+  precmd_functions+=(set_win_title)
+fi
